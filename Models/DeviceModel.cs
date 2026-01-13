1b16a63946b3198dcfbddae766c6f0609e2688d6
LucasErrNotFound <robertxlucas21@protonmail.com>
2026-01-13 14:40:30 +0800

    feat(device-model): add more properties for device-model

--- Models/DeviceModel.cs
+++ Models/DeviceModel.cs
@@ -2,10 +2,14 @@ namespace Signalora.Models;
 
 public class DeviceModel
 {
-    public string Name { get; set; }
-    public string IpAddress { get; set; }
-    public string MacAddress { get; set; }
-    public string Status { get; set; }
-    public string Icon { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }            // iPhone 16 Pro Max, Samsung A14 5G, etc
+    public string IpAddress { get; set; }       // 192.168.1.20, etc
+    public string MacAddress { get; set; }      // 00-1A-2B-3C-4D-5E
+    public string Status { get; set; }          // Connected, Disconnected, etc
+    public string Category { get; set; }        // Phone, Pc, etc
+    public string Connection { get; set; }      // Ethernet, Wireless, etc
+    public string SignalStrength { get; set; }  // Good, Bad, etc
+    public string Icon { get; set; }            // Icon of Device
     public string DeviceInfo => $"{IpAddress} - {MacAddress}";
 }
